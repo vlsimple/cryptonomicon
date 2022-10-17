8f2642cd1964e57d1c78c61322d7f24352182cd0
vlsimple <61254299+vlsimple@users.noreply.github.com>
2022-10-17 08:36:32 +0300

    working api.js

--- src/App.vue
+++ src/App.vue
@@ -263,6 +263,9 @@ export default {
       this.tickers
         .filter((t) => t.name === tickerName)
         .forEach((t) => {
+          if (t === this.selectedTicker) {
+            this.graph.push(price);
+          }
           t.price = price;
         });
     },
